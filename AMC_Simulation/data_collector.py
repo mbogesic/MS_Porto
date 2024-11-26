16405ef9760f50591dffaf53eb80301ce00561df
Chris Schneider <110739072+ChrisKnorri@users.noreply.github.com>
2024-11-26 18:15:44 +0000

    Delete AMC_Simulation directory

--- AMC_Simulation/data_collector.py
+++
@@ -1,8 +0,0 @@
-from mesa.datacollection import DataCollector
-
-def create_data_collector(model):
-    """Creates a DataCollector for the TransportModel."""
-    data_collector = DataCollector(
-        agent_reporters={"CO2 Emissions": "co2_emissions"},  # Adjust based on your agent's attributes
-    )
-    return data_collector
